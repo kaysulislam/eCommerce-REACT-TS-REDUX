1b200833bd0263d99729ab9c2c2701705ba59892
Kaysul Anik <kaysulislam@gmail.com>
2021-12-07 17:22:05 +0200

    fetched sample data using useEffect from jsonplaceholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+
 import AppBar from './components/AppBar/AppBar';
 import Order from './components/Order/Order';
 import ProductAll from './components/ProductAll/ProductAll';
@@ -8,8 +11,23 @@ import LogInIndex from './components/LogIn/LogInIndex';
 import LogInEmail from './components/LogIn/LogInEmail';
 import LogInGoogle from './components/LogIn/LogInGoogle';
 import Register from './components/Register/Register';
+import { State } from './state/index';
+import { fetchProducts } from './state/action-creators/index';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  /*
+  const fetchedProductState = useSelector((state: State) => state.products);
+  const { products } = fetchedProductState;
+  const productData = Object.keys(products).map(item => products[item]);
+  console.log(productData);
+  */
+
   return (
     <div className="App">
       <AppBar />
